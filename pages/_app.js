c8b7a4c67193441688a82d39ed94f947705f0569
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-07-06 00:17:05 -0300

    ajuste de sintaxe

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
       </Head>
       <GlobalStyle />
