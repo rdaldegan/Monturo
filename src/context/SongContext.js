4dca2314be2c479f4be3ff43d65733f299231058
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-07-09 13:37:13 -0300

    final

--- src/context/SongContext.js
+++ src/context/SongContext.js
@@ -3,37 +3,37 @@ import React, { createContext, useContext, useState } from 'react';
 const musicList = [
   {
     id: 1,
-    url: '/tapera jardimcorrégo mix_v2.wav',
+    url: '/musicas/tapera jardimcorrégo mix_v2.wav',
     nome: 'Tapera córrego',
   },
   {
     id: 2,
-    url: '/tapera monturo mixv4.wav',
+    url: '/musicas/tapera monturo mixv4.wav',
     nome: 'Tapera monturo',
   },
   {
     id: 3,
-    url: '/tapera funk_mix v009 Copy.wav',
+    url: '/musicas/tapera funk_mix v009 Copy.wav',
     nome: 'Tapera funk',
   },
   {
     id: 4,
-    url: '/tapera 7 - um barato fundamental V6.wav',
+    url: '/musicas/tapera 7 - um barato fundamental V6.wav',
     nome: 'Tapera barato fundamental',
   },
   {
     id: 5,
-    url: '/Arpejadores_MASTER_24.03.2021.wav',
+    url: '/musicas/Arpejadores_MASTER_24.03.2021.wav',
     nome: 'Tapera arpejadores',
   },
   {
     id: 6,
-    url: '/tudo foi feito pra gente lacrar remix v7.wav',
+    url: '/musicas/tudo foi feito pra gente lacrar remix v7.wav',
     nome: 'Tudo foi feito pra gnt lacrar remix',
   },
   {
     id: 7,
-    url: '/vozes_mix_v003.wav',
+    url: '/musicas/vozes_mix_v003.wav',
     nome: 'Tapera voz',
   },
 ];
